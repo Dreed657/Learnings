ccee2105555e6f9cb89d86f44c206f8d0ff5f582
Dreed657 <ninjakiviWTF@gmail.com>
2020-10-01 14:58:20 +0300

    Added SUS

---
+++ C# Web - September 2020/C# Web Basics/Web Server/Chronometer/Chronometer/Chronometer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private Stopwatch timer;
+
+        public string GetTime => this.timer.Elapsed.ToString();
+        
+        public List<string> Laps { get; }
+
+        public Chronometer()
+        {
+            this.timer = new Stopwatch();
+            this.Laps = new List<string>();
+        }
+
+        public void Start()
+        {
+            this.timer.Start();
+        }
+
+        public void Stop()
+        {
+            this.timer.Stop();
+        }
+
+        public string Lap()
+        {
+            var lap = this.timer.Elapsed;
+            Laps.Add(lap.ToString());
+            return lap.ToString();
+        }
+
+        public void Reset()
+        {
+            this.timer.Reset();
+            this.Laps.Clear();
+        }
+
+        public string GetLaps()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Laps:");
+
+            for (var i = 0; i < Laps.Count; i++)
+            {
+                sb.AppendLine($"{i}. {Laps[i]}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# Web - September 2020/C# Web Basics/Web Server/Chronometer/Runner/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Runner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var chronometer = new Chronometer.Chronometer();
+
+            var command = Console.ReadLine();
+            while (command != "exit")
+            {
+                switch (command)
+                {
+                    case "start":
+                        chronometer.Start();
+                        break;
+                    case "lap":
+                        Console.WriteLine(chronometer.Lap());
+                        break;
+                    case "laps":
+                        Console.WriteLine(chronometer.Laps.Count > 0 ? chronometer.GetLaps() : "Laps: no laps");
+                        break;
+                    case "time":
+                        Console.WriteLine(chronometer.GetTime);
+                        break;
+                    case "reset":
+                        chronometer.Reset();
+                        break;
+                    case "stop":
+                        chronometer.Stop();
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
