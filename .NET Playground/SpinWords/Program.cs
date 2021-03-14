2314d8149068a03eb04954063bb566b04f999dfe
Dreed657 <ninjakiviWTF@gmail.com>
2021-03-14 21:06:00 +0200

    Trial run in code wars

---
+++ .NET Playground/SpinWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SpinWords
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string SpinWords(string sentence)
+        {
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= 5)
+                {
+                    words[i] = new String(words[i].Reverse().ToArray());
+                }
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
---
+++ .NET Playground/WebScraprerTests/SpinWords.Tests/Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using SpinWords;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public static void Test1()
+    {
+        Assert.AreEqual("emocleW", Program.SpinWords("Welcome"));
+    }
+
+    [Test]
+    public static void Test2()
+    {
+        Assert.AreEqual("Hey wollef sroirraw", Program.SpinWords("Hey fellow warriors"));
+    }
+
+    [Test]
+    public static void Test3()
+    {
+        Assert.AreEqual("This is a test", Program.SpinWords("This is a test"));
+    }
+
+    [Test]
+    public static void Test4()
+    {
+        Assert.AreEqual("This is rehtona test", Program.SpinWords("This is another test"));
+    }
+
+    [Test]
+    public static void Test5()
+    {
+        Assert.AreEqual("You are tsomla to the last test", Program.SpinWords("You are almost to the last test"));
+    }
+
+    [Test]
+    public static void Test6()
+    {
+        Assert.AreEqual("Just gniddik ereht is llits one more", Program.SpinWords("Just kidding there is still one more"));
+    }
+}
