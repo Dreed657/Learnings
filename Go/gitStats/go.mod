cb2b0bc454fe52bef271d938c24f88d378673aef
Dreed657 <ninjakiviWTF@gmail.com>
2021-06-12 18:27:17 +0300

    Go Git Stats and Analyzer tool

--- Go/gitStats/go.mod
+++ Go/gitStats/go.mod
@@ -1,3 +1,5 @@
 module gitStats
 
 go 1.16
+
+require gopkg.in/src-d/go-git.v4 v4.13.1
--- Go/gitStats/main.go
+++ Go/gitStats/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"os/user"
 )
 
 func main() {
 	var folder string
 	var email string
-
 	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
 	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
 	flag.Parse()
@@ -21,31 +17,4 @@ func main() {
 	}
 
 	stats(email)
-}
-
-func scan(path string) {
-	fmt.Printf("Found folder:\n\n")
-	repositories := recursiveScanFolder(folder)
-	filePath := getDotFilePath()
-	addNewSliceElementsToFile(filePath, repositories)
-	fmt.Printf("\n\nSuccessfully added\n\n")
-}
-
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func getDotFilePath() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dotFile := usr.HomeDir + "/.gogitlocalstats"
-
-	return dotFile
-}
-
-func stats(email string) {
-	print("stats")
 }
