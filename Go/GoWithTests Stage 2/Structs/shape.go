b47f62c7239bf94aa3005a5c3aee70b7ac3b871b
Dreed657 <ninjakiviWTF@gmail.com>
2021-06-15 01:11:27 +0300

    Go struts methods

--- Go/GoWithTests Stage 2/Structs/shape.go
+++ Go/GoWithTests Stage 2/Structs/shape.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
