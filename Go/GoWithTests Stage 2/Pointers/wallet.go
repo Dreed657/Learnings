387e0d2a873437509da98b03263ae6a75f1dc924
Dreed657 <ninjakiviWTF@gmail.com>
2021-06-21 15:54:45 +0300

    Go pointers errors

--- Go/GoWithTests Stage 2/Pointers/wallet.go
+++ Go/GoWithTests Stage 2/Pointers/wallet.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficienFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficienFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return (*w).balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
